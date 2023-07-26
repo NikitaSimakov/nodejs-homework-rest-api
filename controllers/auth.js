d363f9cce538a07baba7ac862c06b2873b5f4828
NikitaSimakov <mishaaniskin@gmail.com>
2023-07-26 11:04:48 +0300

    Write token to MongoDB after login

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,6 +33,6 @@ export const login = async (req, res) => {
     id: user._id,
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1d" });
-  console.log(token);
+  await User.findOneAndUpdate({ _id: user._id }, { token });
   res.json({ token });
 };
