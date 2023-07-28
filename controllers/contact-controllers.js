feac1bc18d6360dc21a1ba3120331a1108da99e6
NikitaSimakov <mishaaniskin@gmail.com>
2023-07-28 10:04:39 +0300

    Add "token" check in authorization middleware

--- controllers/contact-controllers.js
+++ controllers/contact-controllers.js
@@ -2,11 +2,10 @@ import { HttpError } from "../helpers/HttpError.js";
 import { favoriteSchema, schemaAdd } from "../models/contact.js";
 import { Contact } from "../models/contact.js";
 
-export const getListContacts = async (req, res, next) => {
+export const getListContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { favorite, page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  console.log(skip, page, limit);
   if (favorite) {
     const data = await Contact.find({ owner, favorite: true }).populate(
       "name email phone favorite"
@@ -19,7 +18,7 @@ export const getListContacts = async (req, res, next) => {
   res.json(data);
 };
 
-export const getContact = async (req, res, next) => {
+export const getContact = async (req, res) => {
   const id = req.params.contactId;
   const data = await Contact.findById(id);
   if (!data) {
@@ -28,21 +27,20 @@ export const getContact = async (req, res, next) => {
   res.json(data);
 };
 
-export const postNewContact = async (req, res, next) => {
-  console.log(req.user);
+export const postNewContact = async (req, res) => {
   const { _id: owner } = req.user;
   const data = await Contact.create({ ...req.body, owner });
   res.status(201).json(data);
 };
 
-export const deleteContact = async (req, res, next) => {
+export const deleteContact = async (req, res) => {
   const id = req.params.contactId;
   const deleting = await Contact.deleteOne({ _id: id });
   if (!deleting) return HttpError(404, "Not found");
   return res.json({ message: "contact deleted" });
 };
 
-export const updateContact = async (req, res, next) => {
+export const updateContact = async (req, res) => {
   const id = req.params.contactId;
   const newContact = await Contact.findOneAndUpdate({ _id: id }, req.body, {
     new: true,
@@ -51,9 +49,8 @@ export const updateContact = async (req, res, next) => {
   return res.json(newContact);
 };
 
-export const updateStatusContact = async (req, res, next) => {
+export const updateStatusContact = async (req, res) => {
   const id = req.params.contactId;
-  console.log(req.body);
   const updateStatus = await Contact.findByIdAndUpdate(id, req.body, {
     new: true,
   });
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const { SECRET_KEY } = process.env;
 export const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") next(HttpError(401));
+  if (bearer !== "Bearer" || !token) next(HttpError(401));
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
